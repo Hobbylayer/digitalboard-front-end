8eaac3070514d9d182841b43fbe4dd2896dbd621
j4viermora <javmora04@gmail.com>
2021-03-13 23:23:58 -0400

    configuracion en los reducers

---
+++ src/components/Buttom.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+const BtnSticky = () => {
+
+    return(
+        <>
+            <Button>
+                Login
+            </Button>
+        </>
+    )
+
+};
+
+export {BtnSticky}
